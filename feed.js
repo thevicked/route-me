1a0db4c491fc1b2ae8e70346962615301c2d3616
thevicked <43830139+thevicked@users.noreply.github.com>
2025-06-02 14:59:08 +0100

    Update feed.js

--- feed.js
+++ feed.js
@@ -47,12 +47,9 @@ document.addEventListener("DOMContentLoaded", () => {
         <br>
         <p><strong>Details:</strong> ${p.extra}</p>
         ${Array.isArray(p.images) && p.images.length
-  ? `<div class="post-images> p.images.map(img => `
-      <div style="margin-top: 10px;">
-        <img src="${img.dataUrl}" alt="Post Image" style="max-width: 100%; border-radius: 6px;" />
-        ${img.caption ? `<p style="font-size: 0.85rem; margin-top: 4px;"><em>${img.caption}</em></p>` : ""}
-      </div>
-    `)</div> .join("")
+  ? `<div class="post-images">
+      ${p.images.map(img => `<img src="${img.dataUrl}" alt="Post Image" />`).join("")}
+     </div>`
   : ""}
       </div>
       <div class="route-box">
